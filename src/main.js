1cd54d5d4a020b93e66f32f0c055d7ae8222c38d
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-21 19:09:48 +0530

    router implemented

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter,Routes,Route } from "react-router";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Dashboard from "./components/Dashboard";
+
+function App(){
+    return (
+        <>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/Contact" element={<Contact/>}></Route>
+            <Route path="/Dashboard" element={<Dashboard/>}></Route>
+        </Routes>
+        </BrowserRouter>
+        </>
+    )
+
+}
+const reactRoot=ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(<App/>);
